041482849e4cc3f7553a48fd84f503f5b05255ce
Vinicius Pinheiro Porto <vinicius.porto@santander.co.uk>
2023-02-20 05:27:22 +0000

    feat: fib calc working

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [userInput, setUserInput] = useState<string>("");
+
+  const [displayNumber, setDisplayNumber] = useState(0);
+
+  let fibonacciSequence: number[] = [];
+
+  function handleSubmit(e: any) {
+    e.preventDefault();
+
+    setDisplayNumber(parseInt(userInput));
+  }
+
+  function addNumber(e: { preventDefault: () => void }) {
+    e.preventDefault();
+    setDisplayNumber(displayNumber + 1);
+  }
+
+  function subtractNumber(e: { preventDefault: () => void }) {
+    e.preventDefault();
+    setDisplayNumber(displayNumber - 1);
+  }
+
+  function calculateFibonacci(e: { preventDefault: () => void }) {
+    e.preventDefault();
+
+    for (let i = 0; i <= displayNumber; i++) {
+      if (i === 0 || i === 1) {
+        fibonacciSequence.push(i);
+      } else {
+        fibonacciSequence.push(
+          fibonacciSequence[i - 1] + fibonacciSequence[i - 2]
+        );
+      }
+    }
+    console.log(fibonacciSequence);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Fibonacci hook</h1>
+        <form>
+          <label>
+            Type in a number:
+            <br />
+            <input
+              type="number"
+              autoFocus
+              value={userInput}
+              onChange={(e) => setUserInput(e.target.value)}
+            ></input>
+            <button onClick={handleSubmit}>Submit</button>
+            <p>{displayNumber}</p>
+            <button onClick={addNumber}>+</button>
+            <button onClick={subtractNumber}>-</button>
+            <button onClick={calculateFibonacci}>Calculate</button>
+          </label>
+          <br />
+        </form>
+
+        <p>{fibonacciSequence}</p>
       </header>
     </div>
   );
